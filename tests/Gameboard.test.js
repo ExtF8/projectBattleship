be30320bd0c3ed01c0063a092ad58d10e709625f
ExtF8 <extfield8@gmail.com>
2024-07-20 13:16:21 +0300

    refactored to use before and after each and added all ships

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,17 +1,46 @@
 import { Gameboard } from '../src/modules/classes/Gameboard';
-import { Ship } from '../src/modules/classes/Ship';
+import { Ship, ShipManager } from '../src/modules/classes/Ship';
+import { logGrid } from '../src/utility/logHelper';
 
 describe.only('Gameboard', () => {
-    test('should place a ship at specific coordinates', () => {
-        const gameboard = new Gameboard(1);
-        const carrier = Ship.create(Ship.Types.CARRIER);
-        const patrolBoat = Ship.create(Ship.Types.PATROL_BOAT);
+    let shipManager;
+    let gameboard;
+    let carrier;
+    let battleship;
+    let destroyer;
+    let submarine;
+    let patrolBoat;
 
-        gameboard.placeShip(carrier, 'A', 1, 'horizontal');
-        gameboard.placeShip(patrolBoat, 'B', 2, 'vertical');
+    beforeEach(() => {
+        shipManager = new ShipManager();
+        gameboard = new Gameboard(1);
+        Ship.defaultShipManager = shipManager;
+        carrier = Ship.create(Ship.Types.CARRIER);
+        battleship = Ship.create(Ship.Types.BATTLESHIP);
+        destroyer = Ship.create(Ship.Types.DESTROYER);
+        submarine = Ship.create(Ship.Types.SUBMARINE);
+        patrolBoat = Ship.create(Ship.Types.PATROL_BOAT);
+    });
 
-        expect(gameboard.getShipAt('A', 1)).toBe(carrier);
-        expect(gameboard.getShipAt('B', 2)).toBe(patrolBoat);
+    
 
+    afterEach(() => {
+        logGrid(gameboard.grid);
+    });
+
+    test('should place ships at specific coordinates', () => {
+        gameboard.placeShip(carrier, 'A', 1, 'vertical');
+        gameboard.placeShip(battleship, 'B', 2, 'vertical');
+        gameboard.placeShip(destroyer, 'C', 3, 'vertical')
+        gameboard.placeShip(submarine, 'D', 4, 'vertical');
+        gameboard.placeShip(patrolBoat, 'E', 5, 'vertical');
+
+        expect(gameboard.getShipAt('A', 1).id).toBe(carrier.id);
+        expect(gameboard.getShipAt('B', 2).id).toBe(battleship.id);
+        expect(gameboard.getShipAt('C', 3).id).toBe(destroyer.id)
+        expect(gameboard.getShipAt('D', 4).id).toBe(submarine.id);
+        expect(gameboard.getShipAt('E', 5).id).toBe(patrolBoat.id);
     });
 });
+// test('should receive attack on specified coordinates', () => {});
+
