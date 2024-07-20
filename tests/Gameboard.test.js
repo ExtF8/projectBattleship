4e6f726fa7fba9b6aeb530ec0cbe8b0684dc7ea7
ExtF8 <extfield8@gmail.com>
2024-07-20 15:34:44 +0300

    added tests for attack and refactored to track changes for whole board

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -10,34 +10,48 @@ describe.only('Gameboard', () => {
     let destroyer;
     let submarine;
     let patrolBoat;
+    let attackHit;
+    let attackMiss;
 
-    beforeEach(() => {
+    beforeAll(() => {
         shipManager = new ShipManager();
         gameboard = new Gameboard(1);
         Ship.defaultShipManager = shipManager;
+
+        // place
         carrier = Ship.create(Ship.Types.CARRIER);
+        // receive attack
         battleship = Ship.create(Ship.Types.BATTLESHIP);
+
         destroyer = Ship.create(Ship.Types.DESTROYER);
+
         submarine = Ship.create(Ship.Types.SUBMARINE);
+
         patrolBoat = Ship.create(Ship.Types.PATROL_BOAT);
-    });
 
-    afterEach(() => {
-        logGrid(gameboard.grid);
-    });
-
-    test('should place ships at specific coordinates', () => {
         gameboard.placeShip(carrier, 'A', 1, 'vertical');
         gameboard.placeShip(battleship, 'B', 2, 'vertical');
         gameboard.placeShip(destroyer, 'C', 3, 'vertical');
         gameboard.placeShip(submarine, 'D', 4, 'vertical');
         gameboard.placeShip(patrolBoat, 'E', 5, 'vertical');
 
+        attackHit = gameboard.receiveAttack(['B', 2]);
+        attackMiss = gameboard.receiveAttack(['B', 1]);
+    });
+
+    afterAll(() => {
+        return logGrid(gameboard.grid), console.log(gameboard.missedAttacks);
+    });
+
+    test('should place ships at specific coordinates', () => {
         expect(gameboard.getShipAt('A', 1).id).toBe(carrier.id);
-        expect(gameboard.getShipAt('B', 2).id).toBe(battleship.id);
-        expect(gameboard.getShipAt('C', 3).id).toBe(destroyer.id);
-        expect(gameboard.getShipAt('D', 4).id).toBe(submarine.id);
-        expect(gameboard.getShipAt('E', 5).id).toBe(patrolBoat.id);
+    });
+
+    test('should receive attack on specified coordinates', () => {
+        expect(attackHit).toBe(true);
+    });
+
+    test('should return false if attack was a miss', () => {
+        expect(attackMiss).toBe(false);
     });
 });
-// test('should receive attack on specified coordinates', () => {});
