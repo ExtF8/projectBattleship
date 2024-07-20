582228d2faccd45ce954b2cbd021e51d6999251b
ExtF8 <extfield8@gmail.com>
2024-07-20 09:50:22 +0300

    added tests for ship placement

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -3,13 +3,15 @@ import { Ship } from '../src/modules/classes/Ship';
 
 describe.only('Gameboard', () => {
     test('should place a ship at specific coordinates', () => {
-        // Setup
         const gameboard = new Gameboard(1);
-        const carrier = Ship.create(Ship.Types.CARRIER);
-        // Action
-        gameboard.placeShip(carrier, 'A', 1, 'horizontal');
 
-        // Assertion
+        const carrier = Ship.create(Ship.Types.CARRIER);
+        const patrolBoat = Ship.create(Ship.Types.PATROL_BOAT);
+
+        gameboard.placeShip(carrier, 'A', 1, 'horizontal');
+        gameboard.placeShip(patrolBoat, 'B', 2, 'vertical');
+
         expect(gameboard.getShipAt('A', 1)).toBe(carrier);
+        expect(gameboard.getShipAt('B', 2)).toBe(patrolBoat);
     });
 });
