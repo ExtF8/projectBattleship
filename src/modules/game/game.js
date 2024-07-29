413e68e0a4546842bb918d6e274551fd99777c24
ExtF8 <extfield8@gmail.com>
2024-07-29 19:58:42 +0300

    added files game.js and game.test.js

---
+++ src/modules/game/game.js
@@ -0,0 +1,16 @@
+import { Player } from "../classes/Player";
+
+// Init Players
+
+// Init player boards
+
+// Place ships on a gameboard
+
+// Start game
+
+// Place attacks
+    // log results
+
+// Determine winner
+
+
