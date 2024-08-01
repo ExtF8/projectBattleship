1ed6529147fea1f7c5c606806c4177600ab07b67
Hardijs <78138567+ExtF8@users.noreply.github.com>
2024-08-01 07:57:08 +0300

    refactored tests for Game class implementation

--- tests/game.test.js
+++ tests/game.test.js
@@ -4,17 +4,23 @@ import { initializeGame } from '../src/modules/game/game';
 
 // TODO: write tests for game class
 describe('Game logic', () => {
-    let playerOne;
-    let playerTwo;
+    let game;
+    
     beforeAll(() => {
-        const players = initializeGame();
-
-        playerOne = players.playerOne;
-        playerTwo = players.playerTwo;
+        game = new Game();
     });
 
     test('should initialize players and place ships correctly', () => {
+        game = initializeGame();
+
+        expect(game.playerOne).toBeDefined();
+        expect(game.playerTwo).toBeDefined();
+        
         expect(playerOne.gameboard.getShipAt('A', 1)).toBeInstanceOf(Ship);
         expect(playerTwo.gameboard.getShipAt('J', 1)).toBeInstanceOf(Ship);
+    });
+    test('should handle attacks and update game state', () => {
+        let attackResult = game.playerOne.attack(game.playerTwo, ['J', 1]);
+        expect(attackResult).toBe(true);
     });
 });
