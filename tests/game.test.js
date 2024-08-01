a20a809f828f6588ff1b97e162133dc8b96aee23
Hardijs <78138567+ExtF8@users.noreply.github.com>
2024-08-01 07:38:59 +0300

    added comment in game.test.js file to write tests for Game class

--- tests/game.test.js
+++ tests/game.test.js
@@ -2,6 +2,7 @@ import { Player } from '../src/modules/classes/Player';
 import { Ship } from '../src/modules/classes/Ship';
 import { initializeGame } from '../src/modules/game/game';
 
+// TODO: write tests for game class
 describe('Game logic', () => {
     let playerOne;
     let playerTwo;
