a3a6fd13961e944fa6663d61e5065ac6fd7509f8
ExtF8 <extfield8@gmail.com>
2024-08-01 07:07:18 +0300

    added game init player ship placement test

---
+++ tests/game.test.js
@@ -0,0 +1,19 @@
+import { Player } from '../src/modules/classes/Player';
+import { Ship } from '../src/modules/classes/Ship';
+import { initializeGame } from '../src/modules/game/game';
+
+describe('Game logic', () => {
+    let playerOne;
+    let playerTwo;
+    beforeAll(() => {
+        const players = initializeGame();
+
+        playerOne = players.playerOne;
+        playerTwo = players.playerTwo;
+    });
+
+    test('should initialize players and place ships correctly', () => {
+        expect(playerOne.gameboard.getShipAt('A', 1)).toBeInstanceOf(Ship);
+        expect(playerTwo.gameboard.getShipAt('J', 1)).toBeInstanceOf(Ship);
+    });
+});
