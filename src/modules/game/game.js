07d0e3ef8f22b2bc48325a8a7ba718bc260e64be
ExtF8 <extfield8@gmail.com>
2024-08-02 07:18:06 +0300

    refactored

--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -3,125 +3,114 @@ import { Player } from '../classes/Player';
 import { Ship, ShipManager } from '../classes/Ship';
 
 // TODO: refactor as a class
-// export class Game {
-    // constructor(){
-        // this.playerOne = null;
-        // this.playerTwo = null;
-        // this.currentTurn = null;
-        // this.shipManager = new ShipManager();
-        // Ship.defaultShipManager = this.shipManager;
+export class Game {
+    constructor() {
+        this.playerOne = null;
+        this.playerTwo = null;
+        this.currentTurn = null;
+        this.shipManager = new ShipManager();
+        Ship.defaultShipManager = this.shipManager;
+    }
 
-// }
-    // initializeGame(){
-        // this.playerOne = new Player(1, 'Human', false);
-        // this.playerTwo = new Player(2, 'Computer', true);
-        // this.initShips();
-        // this.placeShips();
-        // this.currentTurn = this.playerOne
-// }
+    initializeGame() {
+        this.playerOne = new Player(1, 'Human', false);
+        this.playerTwo = new Player(2, 'Computer', true);
+        this.initShips();
+        this.placeShips();
+        this.currentTurn = this.playerOne;
 
-// }
+        // logGrid(this.playerOne.gameboard.grid, this.playerOne.gameboard);
+        // logGrid(this.playerTwo.gameboard.grid, this.playerTwo.gameboard);
+    }
 
-// Init Players
-let playerOne;
-let playerTwo;
-let shipManager;
+    // Init ships
+    initShips() {
+        this.playerOneShips = {
+            CARRIER: Ship.create(Ship.Types.CARRIER),
+            BATTLESHIP: Ship.create(Ship.Types.BATTLESHIP),
+            DESTROYER: Ship.create(Ship.Types.DESTROYER),
+            SUBMARINE: Ship.create(Ship.Types.SUBMARINE),
+            PATROL_BOAT: Ship.create(Ship.Types.PATROL_BOAT),
+        };
 
-// Ships
-const PLAYER_ONE_SHIPS = {};
+        this.playerTwoShips = {
+            CARRIER: Ship.create(Ship.Types.CARRIER),
+            BATTLESHIP: Ship.create(Ship.Types.BATTLESHIP),
+            DESTROYER: Ship.create(Ship.Types.DESTROYER),
+            SUBMARINE: Ship.create(Ship.Types.SUBMARINE),
+            PATROL_BOAT: Ship.create(Ship.Types.PATROL_BOAT),
+        };
+    }
 
-const PLAYER_TWO_SHIPS = {};
+    // Place ships
+    placeShips() {
+        // Player One Ships
+        this.playerOne.gameboard.placeShip(
+            this.playerOneShips.CARRIER,
+            'A',
+            1,
+            'vertical'
+        );
+        this.playerOne.gameboard.placeShip(
+            this.playerOneShips.BATTLESHIP,
+            'B',
+            2,
+            'vertical'
+        );
+        this.playerOne.gameboard.placeShip(
+            this.playerOneShips.DESTROYER,
+            'C',
+            3,
+            'vertical'
+        );
+        this.playerOne.gameboard.placeShip(
+            this.playerOneShips.SUBMARINE,
+            'D',
+            4,
+            'vertical'
+        );
+        this.playerOne.gameboard.placeShip(
+            this.playerOneShips.PATROL_BOAT,
+            'E',
+            5,
+            'vertical'
+        );
 
-// Init ships
-function initShips() {
-    shipManager = new ShipManager();
-    Ship.defaultShipManager = shipManager;
+        // Player Two Ships
+        this.playerTwo.gameboard.placeShip(
+            this.playerTwoShips.CARRIER,
+            'J',
+            1,
+            'vertical'
+        );
+        this.playerTwo.gameboard.placeShip(
+            this.playerTwoShips.BATTLESHIP,
+            'I',
+            2,
+            'vertical'
+        );
+        this.playerTwo.gameboard.placeShip(
+            this.playerTwoShips.DESTROYER,
+            'H',
+            3,
+            'vertical'
+        );
+        this.playerTwo.gameboard.placeShip(
+            this.playerTwoShips.SUBMARINE,
+            'G',
+            4,
+            'vertical'
+        );
+        this.playerTwo.gameboard.placeShip(
+            this.playerTwoShips.PATROL_BOAT,
+            'F',
+            5,
+            'vertical'
+        );
 
-    PLAYER_ONE_SHIPS.CARRIER = Ship.create(Ship.Types.CARRIER);
-    PLAYER_ONE_SHIPS.BATTLESHIP = Ship.create(Ship.Types.BATTLESHIP);
-    PLAYER_ONE_SHIPS.DESTROYER = Ship.create(Ship.Types.DESTROYER);
-    PLAYER_ONE_SHIPS.SUBMARINE = Ship.create(Ship.Types.SUBMARINE);
-    PLAYER_ONE_SHIPS.PATROL_BOAT = Ship.create(Ship.Types.PATROL_BOAT);
-
-    PLAYER_TWO_SHIPS.CARRIER = Ship.create(Ship.Types.CARRIER);
-    PLAYER_TWO_SHIPS.BATTLESHIP = Ship.create(Ship.Types.BATTLESHIP);
-    PLAYER_TWO_SHIPS.DESTROYER = Ship.create(Ship.Types.DESTROYER);
-    PLAYER_TWO_SHIPS.SUBMARINE = Ship.create(Ship.Types.SUBMARINE);
-    PLAYER_TWO_SHIPS.PATROL_BOAT = Ship.create(Ship.Types.PATROL_BOAT);
+    }
 }
 
-// Place ships on a gameboard
-function placePlayerOneShips() {
-    playerOne.gameboard.placeShip(PLAYER_ONE_SHIPS.CARRIER, 'A', 1, 'vertical');
-    playerOne.gameboard.placeShip(
-        PLAYER_ONE_SHIPS.BATTLESHIP,
-        'B',
-        2,
-        'vertical'
-    );
-    playerOne.gameboard.placeShip(
-        PLAYER_ONE_SHIPS.DESTROYER,
-        'C',
-        3,
-        'vertical'
-    );
-    playerOne.gameboard.placeShip(
-        PLAYER_ONE_SHIPS.SUBMARINE,
-        'D',
-        4,
-        'vertical'
-    );
-    playerOne.gameboard.placeShip(
-        PLAYER_ONE_SHIPS.PATROL_BOAT,
-        'E',
-        5,
-        'vertical'
-    );
-
-    logGrid(playerOne.gameboard.grid, playerOne.gameboard);
-    // return playerOne.gameboard;
-}
-
-function placePlayerTwoShips() {
-    playerTwo.gameboard.placeShip(PLAYER_TWO_SHIPS.CARRIER, 'J', 1, 'vertical');
-    playerTwo.gameboard.placeShip(
-        PLAYER_TWO_SHIPS.BATTLESHIP,
-        'I',
-        2,
-        'vertical'
-    );
-    playerTwo.gameboard.placeShip(
-        PLAYER_TWO_SHIPS.DESTROYER,
-        'H',
-        3,
-        'vertical'
-    );
-    playerTwo.gameboard.placeShip(
-        PLAYER_TWO_SHIPS.SUBMARINE,
-        'G',
-        4,
-        'vertical'
-    );
-    playerTwo.gameboard.placeShip(
-        PLAYER_TWO_SHIPS.PATROL_BOAT,
-        'F',
-        5,
-        'vertical'
-    );
-
-    logGrid(playerTwo.gameboard.grid, playerTwo.gameboard);
-}
-
-// Start game
-export function initializeGame() {
-    playerOne = new Player(1, 'Human', false);
-    playerTwo = new Player(2, 'Computer', true);
-    initShips();
-    placePlayerOneShips();
-    placePlayerTwoShips();
-
-    return { playerOne, playerTwo };
-}
 
 // Place attacks
 // log results
