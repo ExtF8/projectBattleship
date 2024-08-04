c059e80490a68273f9db3c362989ba0f5aa57383
ExtF8 <extfield8@gmail.com>
2024-08-04 15:30:27 +0300

    refactored tests and logGrid

--- tests/game.test.js
+++ tests/game.test.js
@@ -7,17 +7,19 @@ import { logGrid } from '../src/utility/logHelper';
 describe('Game logic', () => {
     let game;
 
-    beforeAll(() => {
+    beforeEach(() => {
         game = new Game();
         game.initializeGame();
 
         // PlayerOne ships
+        game.placeShipsForPlayer(game.playerOne, 'CARRIER', ['A', 1], 'vertical');
         game.placeShipsForPlayer(game.playerOne, 'BATTLESHIP', ['B', 2], 'vertical');
         game.placeShipsForPlayer(game.playerOne, 'DESTROYER', ['C', 3], 'vertical');
         game.placeShipsForPlayer(game.playerOne, 'SUBMARINE', ['D', 4], 'vertical');
         game.placeShipsForPlayer(game.playerOne, 'PATROL_BOAT', ['E', 5], 'vertical');
 
         // PlayerTwo ships
+        game.placeShipsForPlayer(game.playerTwo, 'CARRIER', ['J', 1], 'vertical');
         game.placeShipsForPlayer(game.playerTwo, 'BATTLESHIP', ['I', 2], 'vertical');
         game.placeShipsForPlayer(game.playerTwo, 'DESTROYER', ['H', 3], 'vertical');
         game.placeShipsForPlayer(game.playerTwo, 'SUBMARINE', ['G', 4], 'vertical');
@@ -25,8 +27,8 @@ describe('Game logic', () => {
     });
 
     afterAll(() => {
-        logGrid(game.playerOne.gameboard.grid, game.playerOne.gameboard);
-        logGrid(game.playerTwo.gameboard.grid, game.playerTwo.gameboard);
+        logGrid(game.playerOne.gameboard);
+        logGrid(game.playerTwo.gameboard);
     });
 
     test('should initialize players', () => {
@@ -35,11 +37,11 @@ describe('Game logic', () => {
     });
 
     test('should place ships for players', () => {
-        game.placeShipsForPlayer(game.playerOne, 'CARRIER', ['A', 1], 'vertical');
-        game.placeShipsForPlayer(game.playerTwo, 'CARRIER', ['J', 1], 'vertical');
+        const playerOneShip = game.playerOne.gameboard.getShipAt(['A', 1]);
+        const playerTwoShip = game.playerTwo.gameboard.getShipAt(['J', 1]);
 
-        expect(game.playerOne.gameboard.getShipAt(['A', 1])).toBeInstanceOf(Ship);
-        expect(game.playerTwo.gameboard.getShipAt(['J', 1])).toBeInstanceOf(Ship);
+        expect(playerOneShip).toBeInstanceOf(Ship);
+        expect(playerTwoShip).toBeInstanceOf(Ship);
     });
 
     test('should handle attacks and update game state', () => {
